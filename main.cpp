62610227b101c8fb678abc39ae7a097f938a3bfb
Kaustubh Shivdikar <kaustubhcs07@gmail.com>
2017-09-01 01:46:05 -0400

    Enumeration Test 6 ~KTB

--- main.cpp
+++ main.cpp
@@ -20,8 +20,8 @@ int main() {
     CCIS student_1 = good;
     COE student_2 = best;
 
-    cout << "This is student 1: "student_1 << endl;
-    cout << "This is student 2: "student_2 << endl;
+    cout << "This is student 1: " << student_1 << endl;
+    cout << "This is student 2: " << student_2 << endl;
 
     cout<< endl << "Press ENTER to exit code...";
     cin.get();
