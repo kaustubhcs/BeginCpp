9a95f5444b07beae916d404049076db846d613b2
Kaustubh Shivdikar <kaustubhcs07@gmail.com>
2017-09-01 00:13:30 -0400

    Class TEST ~KTB

---
+++ classtest.cpp
@@ -0,0 +1,5 @@
+//
+// Created by kaust on 01-09-2017.
+//
+
+#include "classtest.h"
---
+++ classtest.h
@@ -0,0 +1,8 @@
+//
+// Created by kaust on 01-09-2017.
+//
+
+#ifndef BEGINCPP_CLASSTEST_H
+#define BEGINCPP_CLASSTEST_H
+
+#endif //BEGINCPP_CLASSTEST_H
