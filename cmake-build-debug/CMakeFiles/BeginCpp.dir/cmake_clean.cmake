f571e1bbae25b1e0ef91d5b427e81f69ccfb3e88
Kaustubh Shivdikar <7969023+kaustubhcs@users.noreply.github.com>
2017-09-10 02:11:53 -0400

    Auto Commits
    ~KTB

---
+++ cmake-build-debug/CMakeFiles/BeginCpp.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/BeginCpp.dir/main.cpp.o"
+  "CMakeFiles/BeginCpp.dir/classtest.cpp.o"
+  "CMakeFiles/BeginCpp.dir/enumtest.cpp.o"
+  "BeginCpp.pdb"
+  "BeginCpp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/BeginCpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/BeginCpp.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for BeginCpp.
+# This may be replaced when dependencies are built.
